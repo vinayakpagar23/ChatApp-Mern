dfff35d369f75b9e723eabb54eb11bdd99e4ef34
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-01 09:05:29 +0530

    client created

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <>Hello Vk, How r u</>
+    </div>
+  );
+}
+
+export default App;
