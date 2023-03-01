70ddcc97e81714772c1b7a74d2ae60fba1123e0a
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-01 11:07:40 +0530

    server created and first dummy api tetsted

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const {chats} = require("./data/data")
+
+const dotenv =require("dotenv");
+dotenv.config();
+
 
 const app = express();
 
@@ -6,4 +11,15 @@ app.get("/",(req,resp)=>{
     resp.send("Api is Running");
 })
 
+app.get("/api/chats",(req,resp)=>{
+    resp.send(chats);
+})
+
+app.get("/api/chats/:id",(req,resp)=>{
+    // resp.send(req.params.id);
+    const singleChat = chats.find((c)=> c._id === req.params.id);
+    resp.send(singleChat);
+})
+
+const PORT = process.env.PORT || 5000;
 app.listen(5000,console.log("server started"));
