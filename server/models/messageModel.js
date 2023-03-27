834c38bb09516a378681525781f0ccca49f6e9d3
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-27 17:19:15 +0530

    Schema created for userm ,messae,chat

---
+++ server/models/messageModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const messageModel = mongoose.Schema(
+    {
+      sender :{type:mongoose.Schema.Types.ObjectId,ref :"User"},
+      content: {Type:String,trim:true},
+      chat:{type:Mongoose.Schema.Types.ObjectId, ref :"Chat"},
+
+    },
+    {
+        timestamps:true,
+    }
+);
+
+const Message = mongoose.model("Message",MessageModel);
+
+module.exports = Message; 
