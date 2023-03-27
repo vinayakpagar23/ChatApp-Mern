d067e93714435cbc47ead2b5fa1a7b40ec981184
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-28 02:06:00 +0530

    working on mongodb connection

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const {chats} = require("./data/data")
 
 const dotenv =require("dotenv");
+const connectDB = require("./config/db");
 dotenv.config();
 
+connectDB();
 
 const app = express();
 
