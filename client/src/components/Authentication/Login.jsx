53fa58c2c6718e082ba2f8efbc3728566b1c5680
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-28 01:39:18 +0530

    login page Ui done

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
-
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [show, setShow] = useState(false);
 
-export default Login
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+
+  const submitHandler = () => {};
+  return (
+    <VStack spacing={"5px"} color="black">
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="Enter Your Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Your Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Sign Up
+      </Button>
+      <Button
+      variant={"solid"}
+        colorScheme="red"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={()=>{
+          setEmail("guest@example.com");
+          setPassword("123456");
+        }}
+      >
+        Get Guest User Credential
+      </Button>
+    </VStack>
+  );
+};
+
+export default Login;
