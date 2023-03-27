edeba0e755f79240ab0638023b012c8266827e98
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-28 01:25:36 +0530

    signup page UI completed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from "./Authentication/Login";
+import Signup from "./Authentication/Signup";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box
+        bg={"white"}
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        color={"black"}
+        borderWidth={"1px"}
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width={"50%"}>Login</Tab>
+            <Tab width={"50%"}>Sign up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
