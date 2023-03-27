64c13bb9b6c591d44c6f22f9a0d8ed9f64ace587
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-27 20:18:10 +0530

    added

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,10 @@ import Home from './components/Home';
 import ChatPage from './components/ChatPage';
 function App() {
   return (
-      <> 
+      <div className='App'> 
       <Route path ="/" component={Home} exact/>
       <Route path ="/chats" component={ChatPage}/>
-      </>
+      </div>
   
   );
 }
