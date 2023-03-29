d2f5dcf666e7848dad3f30b50852aac8346644e3
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-29 12:19:44 +0530

    working on setting up routes and authentication

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const {chats} = require("./data/data")
-
 const dotenv =require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 dotenv.config();
 
 connectDB();
@@ -13,15 +13,7 @@ app.get("/",(req,resp)=>{
     resp.send("Api is Running");
 })
 
-app.get("/api/chats",(req,resp)=>{
-    resp.send(chats);
-})
-
-app.get("/api/chats/:id",(req,resp)=>{
-    // resp.send(req.params.id);
-    const singleChat = chats.find((c)=> c._id === req.params.id);
-    resp.send(singleChat);
-})
+app.use("/api/user",userRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000,console.log(`server started ${PORT}`));
