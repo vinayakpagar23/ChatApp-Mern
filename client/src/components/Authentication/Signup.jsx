218e0058d871b946f98421f7a553032b183bb732
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-30 02:14:16 +0530

    working on upload image to cloudinary

--- client/src/components/Authentication/Signup.jsx
+++ client/src/components/Authentication/Signup.jsx
@@ -1,4 +1,4 @@
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack,useToast } from '@chakra-ui/react'
 import React, { useState } from 'react'
 
 const Signup = () => {
@@ -9,14 +9,58 @@ const Signup = () => {
   const[confirmpassword,setConfirmpassword] =useState();
   const[pic,setPic]=useState();
   const[loading,setLoading] =useState(false);
-
+  const toast = useToast()
   const handleClick=()=>{
     setShow(!show);
-  }
-
-  const postDetails =(pics)=>{
 
   }
+  const postDetails = (pics) => {
+    setLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: "Please Select an image",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "chat-app");
+      data.append("cloud_name", "vinayakvkp");
+      fetch("https://api.cloudinary.com/v1_1/vinayakvkp/image/upload", {
+        method: "post",
+        body: data,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Authorization, Content-Type",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please Select an image",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+  };
   const submitHandler=()=>{}
   return (
     <VStack spacing={"5px"} color="black">
@@ -55,9 +99,9 @@ const Signup = () => {
       <FormControl id='pic' isRequired>
         <FormLabel>Upload Pic</FormLabel>
    
-        <Input type={"file"} p={1.5} accept="image/*"   onChange={(e)=>setPic(e.target.files[0])}/>
+        <Input type={"file"} p={1.5} accept="image/*"   onChange={(e)=>postDetails(e.target.files[0])}/>
       </FormControl>
-      <Button colorScheme="blue" width="100%" style={{marginTop:15}} onClick={submitHandler}>Sign Up</Button>
+      <Button colorScheme="blue" width="100%" style={{marginTop:15}} onClick={submitHandler} isLoading={loading}>Sign Up</Button>
     </VStack>
   )
 }
