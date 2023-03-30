8d44a5dfe54826847a090fd952706907df04333e
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-30 19:38:53 +0530

    added

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
 const express = require("express");
-const {chats} = require("./data/data")
-const dotenv =require("dotenv");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
-const {notFound, errorHandler} = require("./middleware/errorMiddleware")
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
 
 connectDB();
 
 const app = express();
 app.use(express.json()); // to accept json data
-app.get("/",(req,resp)=>{
-    resp.send("Api is Running");
-})
+app.get("/", (req, resp) => {
+  resp.send("Api is Running");
+});
 
-app.use("/api/user",userRoutes);
+app.use("/api/user", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000,console.log(`server started ${PORT}`));
+app.listen(5000, console.log(`server started ${PORT}`));
