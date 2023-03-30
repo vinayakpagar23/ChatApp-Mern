f167224b6f9030641b6341b7bf0e09abf16752c9
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-30 19:35:39 +0530

    added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,17 +1,17 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-const generateToken = require("../config/generateToken")
+const generateToken = require("../config/generateToken");
 
-const registerUser =asyncHandler(async(req,res)=>{
-const {name,email,password,pic} = req.body;
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
 
-if(!name || !email || !password){
+  if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please Enter all the fields");
-}
-const userExits = await User.findOne({email});
+  }
+  const userExits = await User.findOne({ email });
 
-  if(userExits){
+  if (userExits) {
     res.status(400);
     throw new Error("User Already Exists");
   }
@@ -21,41 +21,37 @@ const userExits = await User.findOne({email});
     email,
     password,
     pic,
-  })
+  });
 
-  if(user){
+  if (user) {
     res.status(201).json({
-        _id:user._id,
-        name:user.name,
-        email:user.email,
-        pic:user.pic,
-        token:generateToken(user._id),
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
     });
-  }
-  else{
+  } else {
     res.status(400);
     throw new Error("Failed to create the User");
   }
-
 });
 
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email Or Password");
+  }
+});
 
-const authUser =asyncHandler(async(req,res)=>{
-    const {email,password} = req.body;
-    const user = await User.findOne({email});
-    if(user && (await user.matchPassword(password))){
-   res.json({
-    _id:user._id,
-    name:user.name,
-    email:user.email,
-    pic:user.pic,
-    token:generateToken(user._id),
-   })
-    }
-    else{
-        res.status(401);
-        throw new Error("Invalid Email Or Password");
-    }
-})
-
-module.exports = {registerUser,authUser};
+module.exports = { registerUser, authUser };
