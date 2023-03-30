6342a65b1fe723b5cc6a3a3dc45dd3238f4f511d
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-31 00:06:49 +0530

    able to create group chat

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const {protect} =require( "../middleware/authMiddleware");
-const {accessChat, fetchChats} = require("../controllers/chatController")
+const {accessChat, fetchChats, createGroupChat} = require("../controllers/chatController")
 const router = express.Router();
 
 router.route("/").post(protect,accessChat);
 router.route("/").get(protect,fetchChats);
-// router.route("/group").post(protect,createGroupChat);
+router.route("/group").post(protect,createGroupChat);
 // router.route("/rename").put(protect,renameGroup);
 // router.route("/groupremove").put(protect,removeFromGroup);
 // router.route("/groupadd").put(protect,addTogroup);
