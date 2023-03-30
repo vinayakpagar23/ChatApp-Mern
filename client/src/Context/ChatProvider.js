da25cfe0ac35072b6941d0fdf0af97ec83e4b168
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-31 00:57:21 +0530

    context api part done

--- client/src/Context/ChatProvider.js
+++ client/src/Context/ChatProvider.js
@@ -1,9 +1,28 @@
-import {createContext} from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
-const ChatContext =createContext();
+const ChatContext = createContext();
 
-const ChatProvider  =({children})=>{
-return <ChatContext.Provider>{children}</ChatContext.Provider>;
-}
+const ChatProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const history = useHistory();
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+
+    if (!userInfo) {
+      history.push("/");
+    }
+  }, [history]);
+
+  return (
+    <ChatContext.Provider value={{ user, setUser }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
 
 export default ChatProvider;
