1ad77d66a97dbfbc2028299cf1e9b6e256d335bd
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-31 01:10:31 +0530

    context api part done

--- client/src/Context/ChatProvider.js
+++ client/src/Context/ChatProvider.js
@@ -1,26 +1,34 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
+
   const [user, setUser] = useState();
+ 
   const history = useHistory();
+
   useEffect(() => {
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
     setUser(userInfo);
 
-    if (!userInfo) {
-      history.push("/");
-    }
+    if (!userInfo) history.push("/");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [history]);
 
   return (
-    <ChatContext.Provider value={{ user, setUser }}>
+    <ChatContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
       {children}
     </ChatContext.Provider>
   );
 };
+
 export const ChatState = () => {
   return useContext(ChatContext);
 };
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Box,
   Container,
@@ -11,7 +11,14 @@ import {
 } from "@chakra-ui/react";
 import Login from "./Authentication/Login";
 import Signup from "./Authentication/Signup";
+import { useHistory } from "react-router-dom";
 const Home = () => {
+  const history = useHistory();
+
+  useEffect(()=>{
+    const user =JSON.parse(localStorage.getItem("userInfo"));
+    // if(user) history.push("/chats");
+  },[history])
   return (
     <Container maxW="xl" centerContent>
       <Box
