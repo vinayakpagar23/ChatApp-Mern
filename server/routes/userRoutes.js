7aa03e90a07d72fb947e0847656d8d5897badf70
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-30 19:48:14 +0530

    chats routes started

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { registerUser, authUser } = require("../controllers/userController");
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(allUsers);
 router.post("/login", authUser);
+
 
 module.exports = router;
