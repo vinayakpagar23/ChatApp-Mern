a3fa0fbd3e7b834fe65df1e7a1880ce1ef345531
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-30 19:36:48 +0530

    added

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,17 +1,16 @@
-
-const notFound=(req,res,next)=>{
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    resizeBy.status(404);
-    next(error);
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  resizeBy.status(404);
+  next(error);
 };
 
-const errorHandler =(err,req,res,next)=>{
-const statusCode = res.statusCode ===200 ? 500: res.statusCode;
-res.staus(statusCode);
-res.json({
-    message:err.message,
-    stack:process.env.NODE_ENV === "production" ? null :err.stack,
-});
-}
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.staus(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
 
-module.exports ={notFound, errorHandler};
+module.exports = { notFound, errorHandler };
