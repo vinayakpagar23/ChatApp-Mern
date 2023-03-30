42faadd10f9100e520fcb191eaaf91f91f87025c
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-31 00:17:20 +0530

    renamegroup route set and tested in postman

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const {protect} =require( "../middleware/authMiddleware");
-const {accessChat, fetchChats, createGroupChat} = require("../controllers/chatController")
+const {accessChat, fetchChats, createGroupChat} = require("../controllers/chatController");
+const { renameGroup } = require("../controllers/userController");
 const router = express.Router();
 
 router.route("/").post(protect,accessChat);
 router.route("/").get(protect,fetchChats);
 router.route("/group").post(protect,createGroupChat);
-// router.route("/rename").put(protect,renameGroup);
+router.route("/rename").put(protect,renameGroup);
 // router.route("/groupremove").put(protect,removeFromGroup);
 // router.route("/groupadd").put(protect,addTogroup);
 
