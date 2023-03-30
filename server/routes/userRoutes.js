daa6757e99afe9c440705f323e50667c3191ce3f
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-30 19:38:32 +0530

    added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const {registerUser,authUser} = require("../controllers/userController");
+const { registerUser, authUser } = require("../controllers/userController");
 const router = express.Router();
 
 router.route("/").post(registerUser);
-router.post("/login", authUser)
+router.post("/login", authUser);
 
-module.exports= router;
+module.exports = router;
