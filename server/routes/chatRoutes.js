0586c809690930cc9c138ce056942000593752cf
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-31 00:24:58 +0530

    remove from the group is working fine and tested in postman

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {protect} =require( "../middleware/authMiddleware");
-const {accessChat, fetchChats, createGroupChat,renameGroup} = require("../controllers/chatController");
+const {accessChat, fetchChats, createGroupChat,renameGroup, removeFromGroup} = require("../controllers/chatController");
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.route("/").post(protect,accessChat);
 router.route("/").get(protect,fetchChats);
 router.route("/group").post(protect,createGroupChat);
 router.route("/rename").put(protect,renameGroup);
-// router.route("/groupremove").put(protect,removeFromGroup);
+router.route("/groupremove").put(protect,removeFromGroup);
 // router.route("/groupadd").put(protect,addTogroup);
 
 module.exports = router;
