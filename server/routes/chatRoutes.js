aa37ea1603ddf5d786187dcc24cfa7f48c314dd6
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-31 00:03:35 +0530

    able to fetch chat for particular user

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const {protect} =require( "../middleware/authMiddleware");
-const {accessChat} = require("../controllers/chatController")
+const {accessChat, fetchChats} = require("../controllers/chatController")
 const router = express.Router();
 
 router.route("/").post(protect,accessChat);
-// router.route("/").get(protect,fetchChats);
+router.route("/").get(protect,fetchChats);
 // router.route("/group").post(protect,createGroupChat);
 // router.route("/rename").put(protect,renameGroup);
 // router.route("/groupremove").put(protect,removeFromGroup);
