028c9a0349b765859f86818c67197301d87f7568
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-30 19:33:05 +0530

    chatpage formatted

--- client/src/components/ChatPage.jsx
+++ client/src/components/ChatPage.jsx
@@ -1,25 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 const ChatPage = () => {
-    const[chats,setChats]=useState([]);
+  const [chats, setChats] = useState([]);
 
-    const fetchChats =async() =>{
-        const {data} = await axios.get("api/chats");
-        console.log(data);
-        setChats(data);
-    }
-    useEffect(()=>{
-       fetchChats();
-    },[])
+  const fetchChats = async () => {
+    const { data } = await axios.get("api/chats");
+    console.log(data);
+    setChats(data);
+  };
+  useEffect(() => {
+    fetchChats();
+  }, []);
   return (
     <div>
-        {
-            chats.map((chat)=>(
-               <p key={chat._id}>{chat.chatName}</p> 
-            ))
-        }
+      {chats.map((chat) => (
+        <p key={chat._id}>{chat.chatName}</p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ChatPage
+export default ChatPage;
