130fe9c479a590b261e127bf9dfa31695a6df6d0
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-03-30 19:38:04 +0530

    added

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-
 const userModel = mongoose.Schema(
-    {
-     name:{type:String, required:true},
-     email:{type:String, required:true, unique:true},
-     password:{type:String,required:true},
-     pic:{type:String,default:"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"}
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    pic: {
+      type: String,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
-    {
-        timestamps:true,
-    }
+  },
+  {
+    timestamps: true,
+  }
 );
 
-userModel.methods.matchPassword = async function(enteredPassword){
-  return await bcrypt.compare(enteredPassword,this.password);
-}
+userModel.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 // before saving the data into dataase want to ecrapt password
-userModel.pre("save",async function (next){
-if(!this.isModified){
+userModel.pre("save", async function (next) {
+  if (!this.isModified) {
     next();
-}
-const salt = await bcrypt.genSalt(10);
-this.password = await bcrypt.hash(this.password,salt);
-})
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-const User = mongoose.model("User",userModel);
+const User = mongoose.model("User", userModel);
 
-module.exports = User; 
+module.exports = User;
