9213e18fe8af980afc447f9f4e6fe0fb60da16d1
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-04-10 13:46:06 +0530

    added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,7 @@ const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email,no_of_travellers, budgetPerPerson } = req.body;
+  const { name, email,password, pic } = req.body;
 
   if (!name || !email || !password) {
     res.status(400);
@@ -19,8 +19,8 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     name,
     email,
-    no_of_travellers,
-    budgetPerPerson,
+    password,
+    pic,
   });
 
   if (user) {
